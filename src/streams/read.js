d8b63aa9629abe795562196f3e6009883529685c
Vera Krasnova <azirafail@mail.ru>
2022-06-05 22:12:11 +0300

    complete streams/read task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,11 @@
+import fs from 'fs';
+import path from 'path';
+import { stdout } from 'process';
+import { pipeline } from 'stream/promises';
+
 export const read = async () => {
-    // Write your code here 
-};
+    const stream =  fs.createReadStream(path.resolve('src', 'streams', 'files', 'fileToRead.txt'));
+    await pipeline(stream, stdout);
+  };
+
+read();
