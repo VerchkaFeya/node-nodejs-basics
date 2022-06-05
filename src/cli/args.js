92edec1f9f0491c832fdd0e0c692721f51ca9f86
Vera Krasnova <azirafail@mail.ru>
2022-06-05 22:01:29 +0300

    complete cli/args task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,15 @@
+import { argv } from 'process';
+
+
 export const parseArgs = () => {
-    // Write your code here 
+		const flags = argv.slice(2).filter(x => x.slice(0, 2) === '--');
+		const result = [];
+		for (let flag of flags) {
+			let flagIndex = argv.indexOf(flag);
+			let value = argv[flagIndex+1];
+			result.push(`${flag.slice(2)} is ${value}`);
+		}
+	console.log(result.join(', '));
 };
+
+parseArgs();
